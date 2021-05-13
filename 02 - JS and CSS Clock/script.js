42b4b38e9a75bef860c13f490e67458acce84219
rwcalinin <rwcalinin@gmail.com>
2021-05-13 13:48:37 +0300

    clock fix

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -7,10 +7,14 @@ setDate();
             hours = now.getHours(),
             secondsArrow = document.querySelector('.second-hand'),
             minutesArrow = document.querySelector('.min-hand'),
-            hoursArrow = document.querySelector('.hour-hand');
-      secondsArrow.style.transform = `rotate(${seconds * 6}deg)`;
-      minutesArrow.style.transform = `rotate(${minutes * 6}deg)`;
-      hoursArrow.style.transform = `rotate(${hours * 6}deg)`;
+            hoursArrow = document.querySelector('.hour-hand'),
+            secondsDegree = seconds * 6 + 90,
+            minutesDegree = minutes * 6 + 90,
+            hoursDegree = hours * 6 + 90;
+
+      secondsArrow.style.transform = `rotate(${secondsDegree}deg)`;
+      minutesArrow.style.transform = `rotate(${minutesDegree}deg)`;
+      hoursArrow.style.transform = `rotate(${hoursDegree}deg)`;
     }
     
     setInterval(setDate, 1000);
